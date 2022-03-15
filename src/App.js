a4601de89490e0882d2011dd6e35da8ba89fcc8b
MPak9 <56807392+MPak9@users.noreply.github.com>
2022-03-15 17:48:48 -0400

    Add PersistLogin to App

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Admin from './components/Admin';
 import Lounge from './components/Lounge';
 import Missing from './components/Missing';
 import RequireAuth from './components/RequireAuth';
+import PersistLogin from './components/PersistLogin';
 import{Routes, Route} from 'react-router-dom';
 
 //careful about publically showing these off 
@@ -31,21 +32,23 @@ function App() {
           <Route path='linkpage' element={<LinkPage />} />
           <Route path='unauthorized' element={<Unauthorized />} />
 
-          {/*We want to protect these routes */}
-          <Route element={<RequireAuth allowedRoles = {[ROLES.User]} />}>
-            <Route path='/' element={<Home />} />
-          </Route>
+          <Route element = {<PersistLogin />}>
+            {/*We want to protect these routes */}
+            <Route element={<RequireAuth allowedRoles = {[ROLES.User]} />}>
+              <Route path='/' element={<Home />} />
+            </Route>
 
-          <Route element={<RequireAuth allowedRoles = {[ROLES.Editor]} />}>
-            <Route path='editor' element={<Editor />} />
-          </Route>
+            <Route element={<RequireAuth allowedRoles = {[ROLES.Editor]} />}>
+              <Route path='editor' element={<Editor />} />
+            </Route>
 
-          <Route element={<RequireAuth allowedRoles = {[ROLES.Admin]} />}>
-            <Route path='admin' element={<Admin />} />
-          </Route>
+            <Route element={<RequireAuth allowedRoles = {[ROLES.Admin]} />}>
+              <Route path='admin' element={<Admin />} />
+            </Route>
 
-          <Route element={<RequireAuth allowedRoles = {[ROLES.Editor, ROLES.Admin]} />}>
-            <Route path='lounge' element={<Lounge />} />
+            <Route element={<RequireAuth allowedRoles = {[ROLES.Editor, ROLES.Admin]} />}>
+              <Route path='lounge' element={<Lounge />} />
+            </Route>
           </Route>
 
           {/*Catch all */}
---
+++ src/components/PersistLogin.js
@@ -0,0 +1,48 @@
+import {Outlet} from "react-router-dom";
+import {useState, useEffect} from 'react';
+import useRefreshToken from "../hooks/useRefreshToken";
+import useAuth from "../hooks/useAuth";
+
+
+export const PersistLogin = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const refresh = useRefreshToken();
+  const {auth} = useAuth();
+
+  useEffect(() => {
+    const verifyRefreshToken = async() => {
+        try{
+            await refresh();
+        }
+        catch(err){
+            console.error(err);
+        }
+        finally {
+            setIsLoading(false);
+        }
+    }
+
+    //If we do not have an accesstoken
+    !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
+
+  }, [])
+
+  useEffect (() => {
+    console.log(`isLoading: ${isLoading}`);
+    console.log(`aT: ${JSON.stringify(auth?.accessToken)}`);
+
+  }, [isLoading]);
+
+  return (
+      <>
+        {isLoading
+            ? <p>Loading...</p>
+            : <Outlet />
+        
+        }
+      </>
+  )
+    
+}
+
+export default PersistLogin; 
