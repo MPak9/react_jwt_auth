762c1d5669accb9b80f9770b25f691b490ad6e21
MPak9 <56807392+MPak9@users.noreply.github.com>
2022-03-16 12:35:04 -0400

    useLocalStorage hook

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,8 @@
 import { useRef, useState, useEffect } from 'react';
 import useAuth from '../hooks/useAuth';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
+import useLocalStorage from '../hooks/useLocalStorage';
+
 
 import axios from '../api/axios';
 const LOGIN_URL = '/auth';
@@ -15,7 +17,7 @@ const Login = () => {
     const userRef = useRef();
     const errRef = useRef();
 
-    const [user, setUser] = useState('');
+    const [user, setUser] = useLocalStorage('user', '')//useState('');
     const [pwd, setPwd] = useState('');
     const [errMsg, setErrMsg] = useState('');
 
